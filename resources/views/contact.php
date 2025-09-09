3fc3836e01425fccaecd06ba01a305b25bab463e
nearrindell <nathaniaarrindell3@gmail.com>
2025-09-09 13:54:59 +0200

    Dag 2: Home, about en extra contact page toegevoegd.

---
+++ resources/views/contact.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>hi from nathania contact page </br>
+    Huiswerk uit video 2!: Maak contact page op zelfde manier als about en home page. #Complete!
+    </h1>
+</body>
+</html>
