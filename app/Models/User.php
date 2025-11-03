c0f900cc4e19558de97489e60ea8f17ec3eaafda
nearrindell <nathaniaarrindell3@gmail.com>
2025-11-03 06:11:05 +0100

    Video-22: Made a Login and Registration System From Scratch:Part-2(Real part 2)

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,17 +11,9 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    protected $guarded = [];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+
 
     /**
      * The attributes that should be hidden for serialization.
--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-<script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body class="h-full">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="h-full">
@@ -29,10 +29,18 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
- @guest
+                            @guest
                                 <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
                                 <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
                             @endguest
+
+                            @auth
+                                    <form method="POST" action="/logout">
+                                        @csrf
+
+                                        <x-form-button>Log Out</x-form-button>
+                                    </form>
+                            @endauth
                         </div>
                     </div>
                     <div class="-mr-2 flex md:hidden">
@@ -54,36 +62,9 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
                             </svg>
                         </button>
-
-
-
-
-
-
-
-
-
-
-
                     </div>
                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             </div>
-
 
             <!-- Mobile menu, show/hide based on menu state. -->
             <div class="md:hidden" id="mobile-menu">
@@ -117,23 +98,13 @@
                             </svg>
                         </button>
                     </div>
-
-
-
-
-
-
-
                 </div>
             </div>
         </nav>
 
-
         <header class="bg-white shadow">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
                 <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $heading }}</h1>
-
-
 
                 <x-button href="/jobs/create">Create Job</x-button>
             </div>
