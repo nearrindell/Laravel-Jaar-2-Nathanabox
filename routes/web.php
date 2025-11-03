506386aac16d0bcb234adc30bbd2c8be0b379f2f
nearrindell <nathaniaarrindell3@gmail.com>
2025-11-03 01:08:15 +0100

    Video-18: Editing, Updating and Deleting a Resource

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,34 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
-
 
 Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function () {
-$jobs = Job::with('employer')->latest()->simplePaginate(3);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
     return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function () {
     request()->validate([
         'title' => ['required', 'min:3'],
@@ -39,3 +43,43 @@ Route::post('/jobs', function () {
 
     return redirect('/jobs');
 });
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    // authorize (On hold...)
+
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold...)
+
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
